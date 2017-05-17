814cd4f29ce6f3ce18d8d4193aa353c956496439
yuhuajian <yuhuajian@bytedance.com>
2017-05-17 15:20:17 +0800

    change
    Change-Id: Iedd22fc500de36b0237937c0ea1b115732cce64f

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,15 +1,16 @@
 import React from 'react';
 class Header extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
 
-		}
-	}
-	render() {
-		return (
+    }
+  }
+
+  render() {
+    return (
             <div>{this.props.musicName}</div>
-		)
-	}
+    )
+  }
 }
 export default Header;
--- src/js/page/app.js
+++ src/js/page/app.js
@@ -4,26 +4,31 @@ import { connect } from 'react-redux';
 import Header from '../components/header'
 import * as actions from '.././redux/action/appAction';
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.setvalue = this.setvalue.bind(this);
-	}
-	setvalue(e) {
-		let value = e.target.value;
-		this.props.actions.setMusicName(value);
-		console.log(this.props);
-	}
-	render() {
-		return (
+  constructor(props) {
+    super(props);
+    this.setvalue = this.setvalue.bind(this);
+  }
+  setvalue(e) {
+    let value = e.target.value;
+    this.props.actions.setMusicName(value);
+  }
+  getprop(props = this.props) {
+    console.log(props);
+  }
+  componentDidMount() {
+    this.getprop();
+  }
+  render() {
+    return (
             <div onClick={this.setvalue}>
                 <Header musicName={this.props.musicName} ></Header>
             </div>
-		);
-	}
+    );
+  }
 }
 App = connect((state) => state, (dispatch) => {
-	return {
+  return {
 	    actions: bindActionCreators(actions, dispatch)
-	}
+  }
 } )(App);
 export default App
